1a13d13c587fa673b84808d924311df539be3906
kaique.ferreira <kaique.ferreira.terc@preventsenior.com.br>
2025-12-16 18:01:00 -0300

    feat(ATLAS-0001): Create notification service

---
+++ projects/atlas-ui-notifications/src/lib/atlas-ui-notifications.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'atlas-atlas-ui-notifications',
+  imports: [],
+  template: `
+    <p>
+      atlas-ui-notifications works!
+    </p>
+  `,
+  styles: ``
+})
+export class AtlasUiNotificationsComponent {
+
+}
