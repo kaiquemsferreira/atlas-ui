42c67b1809a5133c16860a2378da7a323ecfa37c
kaique.ferreira <kaique.ferreira.terc@preventsenior.com.br>
2026-01-10 22:21:40 -0300

    feat(ATLAS-0001): Create toast component and home page

---
+++ projects/sandbox/src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: [
+    './home.component.scss'
+  ]
+})
+export class HomeComponent {
+
+}
